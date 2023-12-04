12fbb24d11238c7710b59505c3951ed6c6e6bd06
devboss0924 <smallstar0924@gmail.com>
2023-12-04 15:43:52 +0400

    fixed

--- printCell.py
+++ printCell.py
@@ -2,22 +2,22 @@ import openpyxl
 
 # Load the Excel file
 excel_file = "home-hill_R7andR8_20231128.xlsx"  # Replace with the path to your Excel file
-workbook = openpyxl.load_workbook(excel_file)
+workbook = openpyxl.load_workbook(excel_file, data_only=True)
 # Select the desired worksheet (assuming it's the first sheet)
 worksheet = workbook.active
 
-# Get the formula of cell A16
-cell_formula = worksheet['A16'].value
-cell_formula = cell_formula.replace("=", "")
-print("cell formula", cell_formula)
+# # Get the formula of cell A16
+# cell_formula = worksheet['A16'].value
+# cell_formula = cell_formula.replace("=", "")
+# print("cell formula", cell_formula)
 
-# Define the absolute cell reference
-absolute_reference = cell_formula
+# # Define the absolute cell reference
+# absolute_reference = cell_formula
 
-# Get the cell value using the absolute reference
-cell_value = worksheet[absolute_reference].value
+# # Get the cell value using the absolute reference
+# cell_value = worksheet[absolute_reference].value
 
-# cell_value = worksheet['A16'].value
+cell_value = worksheet['C16'].value
 
 # Print the cell value
 print("Value of cell A16:", cell_value)
